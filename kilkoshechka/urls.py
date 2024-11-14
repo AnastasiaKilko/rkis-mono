25948c7879533d58593074ca4a15b623dd3c8b64
Анастасия Килко <gr431_kiava@lab-403-2.tomtit-tomsk.ru>
2024-11-14 19:21:45 +0700

    fix

--- kilkoshechka/urls.py
+++ kilkoshechka/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:page>/', views.other_page, name='other'),
-    path('account/login', views.BBLoginView.as_view(), name='login'),
-    path('account/profile/', views.profile, name='profile'),
+    path('accounts/login', views.BBLoginView.as_view(), name='login'),
+    path('accounts/profile/change/', views.ChangeUserInfoView.as_view(), name='profile_change'),
+    path('accounts/profile/', views.profile, name='profile'),
     path('acconts/logout/', views.BBLogoutView.as_view(), name='logout')
 ]
--- kilkoshechka/views.py
+++ kilkoshechka/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
@@ -6,7 +6,11 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LogoutView
-
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+from .forms import ChangeUserInfoForm
+from .models import AdvUser
 
 # Create your views here.
 
@@ -33,3 +37,20 @@ def profile(request):
 
 class BBLogoutView(LoginRequiredMixin, LogoutView):
     template_name = 'main/logout.html'
+
+
+class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
+    model = AdvUser
+    template_name = 'main/change_user_info.html'
+    form_class = ChangeUserInfoForm
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Личные данные пользователя изменены'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
